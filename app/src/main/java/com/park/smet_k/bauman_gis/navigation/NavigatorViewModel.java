67150bb7abbbed3883efb0ca1df625c0f07461e8
smet_k <ski97@mail.ru>
2019-12-22 18:58:57 +0300

    view model

--- app/src/main/java/com/park/smet_k/bauman_gis/navigation/NavigatorViewModel.java
+++ app/src/main/java/com/park/smet_k/bauman_gis/navigation/NavigatorViewModel.java
@@ -1,5 +1,26 @@
 package com.park.smet_k.bauman_gis.navigation;
 
-public class NavigatorViewModel {
-    private
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.park.smet_k.bauman_gis.model.GoRoute;
+
+public class NavigatorViewModel extends AndroidViewModel {
+    private NavigatorRepository mNavRepo = new NavigatorRepository(getApplication());
+    private LiveData<GoRoute> mRoute = mNavRepo.getRoute();
+
+    public NavigatorViewModel(@NonNull Application application) {
+        super(application);
+    }
+
+    public LiveData<GoRoute> getNews() {
+        return mRoute;
+    }
+
+    public void find(String from, String to) {
+        mNavRepo.find(from, to);
+    }
 }
