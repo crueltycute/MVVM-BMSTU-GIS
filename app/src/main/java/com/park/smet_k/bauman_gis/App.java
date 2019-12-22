27f6a72c617adc903c0320527a318fb5ca52343d
smet_k <ski97@mail.ru>
2019-12-22 18:00:47 +0300

    seatch api repo

--- app/src/main/java/com/park/smet_k/bauman_gis/App.java
+++ app/src/main/java/com/park/smet_k/bauman_gis/App.java
@@ -4,16 +4,19 @@ import android.app.Application;
 import android.content.Context;
 
 import com.park.smet_k.bauman_gis.api.ApiRepository;
+import com.park.smet_k.bauman_gis.api.SearchApiRepository;
 import com.park.smet_k.bauman_gis.prefs.PreferencesRepository;
 
 public class App extends Application {
     private ApiRepository mApiRepo;
+    private SearchApiRepository mSearchApiRepo;
     private PreferencesRepository mPreferencesRepo;
 
     @Override
     public void onCreate() {
         super.onCreate();
         mApiRepo = new ApiRepository();
+        mSearchApiRepo = new SearchApiRepository();
         mPreferencesRepo = new PreferencesRepository(getApplicationContext());
     }
 
@@ -23,6 +26,10 @@ public class App extends Application {
 
     public ApiRepository getApiRepo() {
         return mApiRepo;
+    }
+
+    public SearchApiRepository getmSearchApiRepo() {
+        return mSearchApiRepo;
     }
 
     public PreferencesRepository getPreferencesRepo() {
---
+++ app/src/main/java/com/park/smet_k/bauman_gis/api/SearchApiRepository.java
@@ -0,0 +1,40 @@
+package com.park.smet_k.bauman_gis.api;
+
+import android.content.Context;
+
+import com.park.smet_k.bauman_gis.App;
+
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.moshi.MoshiConverterFactory;
+
+public class SearchApiRepository {
+    private final SearchApi mAPI;
+    private final OkHttpClient mOkHttpClient;
+
+    public SearchApiRepository() {
+        mOkHttpClient = new OkHttpClient()
+                .newBuilder()
+                .build();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .addConverterFactory(MoshiConverterFactory.create())
+                .baseUrl(new HttpUrl.Builder()
+                        .scheme("https")
+                        .host("95.163.214.14:8000")
+                        .build())
+                .client(mOkHttpClient)
+                .build();
+
+        mAPI = retrofit.create(SearchApi.class);
+    }
+
+    public SearchApi getAPI() {
+        return mAPI;
+    }
+
+    public static ApiRepository from(Context context) {
+        return App.from(context).getApiRepo();
+    }
+}
